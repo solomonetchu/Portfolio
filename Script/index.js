4ef52e1144eff2ab9f7ad15b0f3b7cf8941c3ab3
unknown <solomonetchu@gmail.com>
2022-10-13 02:55:33 -0700

    Added Modal window for Desktop and phone version

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,4 @@
+"use strict";
 const hamburger = document.querySelector(".hmburger");
 const navMenu = document.querySelector(".nav__mobile");
 
@@ -12,3 +13,45 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+// modal Window Display
+const card = document.querySelector(".card");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnCloseMB = document.querySelector(".close-card");
+const btnsOpenModal = document.querySelectorAll(".btn");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  card.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  hamburger.classList.add("hidden");
+};
+const closeMB = function () {
+  card.classList.add("hidden");
+  overlay.classList.add("hidden");
+  hamburger.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+btnCloseMB.addEventListener("click", closeMB);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// Mobile version of Overlay
